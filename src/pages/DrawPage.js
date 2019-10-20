ad55e3581ccf23711038800a260cf346033c3c29
Josep Perna <josepperna@gmail.com>
2019-10-20 11:05:14 +0100

    a

--- src/pages/DrawPage.js
+++ src/pages/DrawPage.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import {Redirect} from 'react-router'
-
+import $ from 'jquery'
 import './DrawPage.scss'
+
+function postToDB(user, functions){
+    var data = {
+        "TableName": "mainBackendTable",
+        "Item": {
+            "drawingID": {
+                "S": user + functions
+            },
+            "user": {
+                "S": user
+            },
+            "functions": {
+                "S": functions  
+                    
+            }
+        }
+    }
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://pjgf4yqxo7.execute-api.eu-west-3.amazonaws.com/default/hackBackend",
+        "method": "POST",
+        "headers": {
+          "Content-Type": "application/json",   
+        },
+        "processData": false,
+        "data": JSON.stringify(data)
+      }
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
+}
 
 class DrawPage extends Component {
     constructor(props) {
@@ -20,6 +53,7 @@ class DrawPage extends Component {
         this.setState({value:e.target.value});
     }
     render() {
+        postToDB("joan", "f(x),y,z")
         if (this.state.redirect) {
             return <Redirect to={{
                         pathname: '/draw/2',
